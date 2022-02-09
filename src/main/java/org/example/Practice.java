02eae90c740549cd9651c912a7914fdb385a7624
Dim98 <dimplemotwani331@gmail.com>
2022-02-09 15:46:47 +0530

    added one commit

--- src/main/java/org/example/Practice.java
+++ src/main/java/org/example/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello from practice");
         //practising git
+        //changes are now from feature.
     }
 }
